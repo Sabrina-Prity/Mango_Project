e6ecd32b0a386da4b2e77a638bfa0e14a7396d3c
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-18 19:11:07 +0600

    payment added2

--- sweet_haven/urls.py
+++ sweet_haven/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('add_to_cart/', include('add_to_cart.urls')),
     path('category/', include('category.urls')),
     path('product/', include('product.urls')),
+    path('payment/', include('payment.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
