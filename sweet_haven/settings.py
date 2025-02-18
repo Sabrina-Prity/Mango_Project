52e26dcf9ffc02dcdede4417fa5f86193470a7b8
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-18 19:50:01 +0600

    payment added3

--- sweet_haven/settings.py
+++ sweet_haven/settings.py
@@ -182,14 +182,10 @@ USE_TZ = True
 
 
 
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),  # Ensure this directory exists
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # WSGI_APPLICATION = 'sweet_haven.wsgi.application'
 
