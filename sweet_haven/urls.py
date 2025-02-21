12b47c737eab5edc8e5ba4a6aed3465b4789602d
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-21 19:16:53 +0600

    update

--- sweet_haven/urls.py
+++ sweet_haven/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('category/', include('category.urls')),
     path('product/', include('product.urls')),
     path('payment/', include('payment.urls')),
-    path('', home),
+    path('/', home),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
