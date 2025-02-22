0427d441b11ae0af1f2c4b8f7e7b33189cbfbad4
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-22 18:57:16 +0600

    updated

--- add_to_cart/serializers.py
+++ add_to_cart/serializers.py
@@ -44,3 +44,10 @@ class OrderGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Order
         fields = '__all__'
+
+class OrderPostSerializer(serializers.ModelSerializer):
+    payment_status = serializers.CharField(source="payment.payment_status", read_only=True)
+
+    class Meta:
+        model = models.Order
+        fields = ['id', 'user', 'product', 'quantity', 'buying_status', 'purchased_at', 'payment', 'payment_status']
