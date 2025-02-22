4e681404f09ef08cb7b6c4a0ba5c4b0e6deb733e
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-22 19:19:03 +0600

    updated

--- add_to_cart/serializers.py
+++ add_to_cart/serializers.py
@@ -45,12 +45,21 @@ class OrderSerializer(serializers.ModelSerializer):
         model = models.Order
         fields = ['id', 'user', 'product', 'quantity', 'buying_status', 'purchased_at', 'payment', 'payment_status']
 
-
 class OrderGetSerializer(serializers.ModelSerializer):
-    product=MangoSerializer()
+    payment_status = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Order
-        fields = '__all__'
+        fields = ['id', 'quantity', 'buying_status', 'purchased_at', 'product', 'user', 'payment_status']
+
+    def get_payment_status(self, obj):
+        return obj.payment.payment_status if obj.payment else "Not Available"
+
+# class OrderGetSerializer(serializers.ModelSerializer):
+#     product=MangoSerializer()
+#     class Meta:
+#         model = models.Order
+#         fields = '__all__'
 
 # class OrderPostSerializer(serializers.ModelSerializer):
 #     payment_status = serializers.CharField(source="payment.payment_status", read_only=True)
